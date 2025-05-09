05110f19576357022ba5a72c513821dd296f82bf
rahmate2003 <w.rahmad26@gmail.com>
2025-05-09 10:24:23 +0800

    Iya Saya

--- src/components/LinkTreeClone.jsx
+++ src/components/LinkTreeClone.jsx
@@ -58,7 +58,7 @@ const LinktreeClone = () => {
                     <div className="mb-4 relative inline-block group">
                         <div className="w-40 h-40 md:w-48 md:h-48 rounded-full overflow-hidden border-4 border-white/20 shadow-2xl mx-auto transform transition duration-500 group-hover:scale-105 relative z-10">
                             <img
-                                src="/profile.png"
+                                src="/profiles.png"
                                 alt="Rahmat Wahyudi"
                                 className="w-full h-full object-cover transition duration-500 group-hover:rotate-6"
                             />
